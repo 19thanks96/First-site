5fbb6bb95bcdaa9d21eddf9c9afb3783aefa5891
hellgamer <19thanks96@gmail.com>
2023-03-08 15:13:15 +0200

    delete empty space

--- js/scriptone.js
+++ js/scriptone.js
@@ -1,5 +1,7 @@
-"use strict"
-
+"use strict";
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
 const isMobile = {
     Android: function() {
         return navigator.userAgent.match(/Android/i);
@@ -21,7 +23,6 @@ const isMobile = {
     }
 };
 
-
 if (isMobile.any()) {
     document.body.classList.add('_touch');
     let menuArrows = document.querySelectorAll('.menu__arrow');
@@ -36,40 +37,32 @@ if (isMobile.any()) {
 } else {
     document.body.classList.add('_pc');
 }
-console.log(isMobile);
 
-//scrolling clicks
-
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
 if ( menuLinks.length > 0) {
     menuLinks.forEach(menuLink => {
         menuLink.addEventListener('click', onMenuClick);
-    });
-
+});
 
 function onMenuClick(e) {
     const menuLink = e.target;
     if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
         const gotoBlock = document.querySelector(menuLink.dataset.goto);
         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-    
         if (iconMenu.classList.contains('_active')) {
             document.body.classList.remove('_lock');
         iconMenu.classList.remove('_active');
         menuBody.classList.remove('_active');
         }
-
         window.scrollTo({
             top: gotoBlockValue,
             behavior: 'smooth'
         });
         e.preventDefault();
-    }
+        }
     }
 }
-// menu burger
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
+
+
 if (iconMenu) {
     iconMenu.addEventListener('click', function (e) {
         document.body.classList.toggle('_lock');
